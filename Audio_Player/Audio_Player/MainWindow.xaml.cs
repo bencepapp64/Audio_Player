12db99dac9badfcfdb9e0a27ed5f42949aeb8aa3
bence <bence@DESKTOP-HO6AV4B>
2021-02-03 10:05:56 +0100

    Open & Play click done.

--- Audio_Player/Audio_Player/MainWindow.xaml.cs
+++ Audio_Player/Audio_Player/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace Audio_Player
 {
@@ -20,9 +22,63 @@ namespace Audio_Player
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> musics = new List<string>();
+        MediaPlayer mp = new MediaPlayer();
+        public bool k = true;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {   
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Title = "Open File";
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            openFileDialog.ShowDialog();
+
+            foreach (var item in openFileDialog.FileNames)
+            {
+                musics.Add(item);
+                List_Box.Items.Add(item);
+            }
+            try
+            {
+                mp.Open(new Uri(musics[0]));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                MessageBox.Show("Please open a file.");
+            }
+            
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (k == false)
+            {
+                mp.Pause();
+                Play.Content = "Play";
+                k = true;
+            }
+            else {
+                mp.Play();
+                Play.Content = "Pause";
+                k = false;
+            }
+        }
+
+        private void Open2_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
